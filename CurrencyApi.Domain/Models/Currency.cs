1ea529c5b9334cc03024d3147c4250d8c12027b4
netordot <sdenisdv1@gmail.com>
2024-08-20 14:33:26 +0300

    Added minimal Domain layer

---
+++ CurrencyApi.Domain/Models/Currency.cs
@@ -0,0 +1,41 @@
+﻿namespace CurrencyApi.Domain.Models
+{
+    public class Currency
+    {
+
+        private Currency(Guid id, string code, string fullname, string sign)
+        {
+            Id = id;
+            Code = code;
+            FullName = fullname;
+            Sign = sign;
+
+        }
+
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Sign { get; set; }
+
+
+
+        public static (Currency currency, string Error) Create(Guid id, string code, string fullname, string sign)
+        {
+            var newCurrency = new Currency(id, code, fullname, sign);
+
+            var error = string.Empty;
+
+            if (string.IsNullOrEmpty(fullname))
+            {
+                error = "The name of the currency should not be empty";
+            }
+
+            return (newCurrency, error);
+
+        }
+
+    }
+}
---
+++ CurrencyApi.Domain/Models/ExchangeRate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyApi.Domain.Models
+{
+    public class ExchangeRate
+    {
+
+        public ExchangeRate(int id, int baseCurrencyId, int targetCurrencyId, decimal rate)
+        {
+            Id = id;
+            BaseCurrencyId = baseCurrencyId;
+            TargetCurrencyId = targetCurrencyId;
+            Rate = rate;
+            
+        }
+        public int Id { get; set; }
+
+        public int BaseCurrencyId { get; set; }
+
+        public int TargetCurrencyId { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public static(ExchangeRate ExRate, string Error) Create (int id, int baseCurrencyId, int targetCurrencyId, decimal rate)
+        {
+            var ExRate = new ExchangeRate(id, baseCurrencyId, targetCurrencyId, rate);
+
+            var error = string.Empty;
+
+            if(baseCurrencyId == targetCurrencyId)
+            {
+                error = "The ExchangeRate may not be created due to the same base and target currencies";
+            }
+
+            if(rate <= 0)
+            {
+                error = "The rate may not be less or equal than 0";
+            }
+
+
+            return (ExRate,  error);
+        }
+
+
+    }
+}
